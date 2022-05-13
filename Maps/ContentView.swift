11922ce4ccb5bc6e3477772da19a2f1682b484ea
Bikram Aryal <bikram.aryall@gmail.com>
2022-05-13 18:52:00 +0545

    Initial Commit

---
+++ Maps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Maps
+//
+//  Created by Bikram Aryal on 13/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Maps/MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsApp.swift
+//  Maps
+//
+//  Created by Bikram Aryal on 13/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
